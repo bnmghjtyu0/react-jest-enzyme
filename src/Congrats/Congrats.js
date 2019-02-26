11b520dfd6a8742c8648255a38e4249bdf14329e
RichardLiao <bnmghjtyu0@gmail.com>
2019-02-26 03:56:16 +0000

    Congrats

---
+++ src/Congrats/Congrats.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+/**
+  * Functional react component for congratulatory message.
+  * @function
+  * @param {object} props - React props.
+  * @returns {JSX.Element} - Rendered component (or null if `success` prop)
+  */
+
+const Congrats = (props) => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+              </span>
+            </div>
+        )
+    } else {
+        return (
+            <div data-test="component-congrats" />
+        )
+    }
+}
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired,
+}
+export default Congrats;
---
+++ src/test/testUtils.js
@@ -0,0 +1,21 @@
+import checkPropTypes from 'check-prop-types'
+
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute of for search.
+ * @returns {ShallowWraper}
+ */
+
+export const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test="${val}"]`);
+}
+
+export const checkProps = (component, conformingProps) => {
+    const propError = checkPropTypes(
+        component.propTypes,
+        conformingProps,
+        'prop',
+        component.name);
+    expect(propError).toBeUndefined()
+}
