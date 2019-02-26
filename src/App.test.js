bb57f76f26390c927451ce00bb96c341b3ec4edc
RichardLiao <bnmghjtyu0@gmail.com>
2019-02-26 03:16:03 +0000

    install: enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('render without crashing', () => {
+    const wrapper = shallow(<App />)
+     console.log(wrapper.debug())
+})
