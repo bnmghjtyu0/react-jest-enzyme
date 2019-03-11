84b6731512357841b3cb2c9ca9f9e97643dc9fa9
RichardLiao <bnmghjtyu0@gmail.com>
2019-03-11 03:05:03 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from './Button/Button';
-import Congrats from './Congrats/Congrats';
-import GuessedWords from './GuessedWords/GuessedWords';
 class App extends Component {
-    constructor(props) {
-        super(props)
-    }
-    handleClose = () => {
-        console.log(123)
-    }
     render() {
         return (
-            <div className="App">
-                <Button onClose={this.handleClose}>123</Button>
-                <Congrats success={true} />
-                <GuessedWords
-                    guessedWords={
-                        [{ guessedWord: 'train', letterMatchCount: 3 }]
-                    }
-                />
-            </div>
+            <div className="App"> </div>
         );
     }
 }
