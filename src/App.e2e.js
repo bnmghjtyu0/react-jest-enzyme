e22bbfadcc4a2e00cbd28af6c5da70ff66da032e
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2019-03-12 20:45:03 +0800

    ok

--- src/App.e2e.js
+++ src/App.e2e.js
@@ -1,24 +1,3 @@
-## 測試 Button
-
-
-### 安裝
-```
-npm install --save jest-puppeteer puppeteer jest
-```
-### 修改 package.json
-
-```json
-    "jest": {
-        "testMatch": [
-             "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
-           - "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}"
-           + "<rootDir>/src/**/?(*.)(spec|test|e2e).{js,jsx,ts,tsx}"
-        ],
-    }
-```
-### App.e2e.js
-
-```js
 import puppeteer from "puppeteer";
 
 describe("Google", () => {
@@ -39,12 +18,3 @@ describe("Google", () => {
     });
   });
 });
-
-```
-
-
-### 執行測試
-
-```
-npm run test
-```
