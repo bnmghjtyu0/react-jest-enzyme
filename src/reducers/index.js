e5301767777f049b8240286dfd9d2ed2adf7ac98
RichardLiao <bnmghjtyu0@gmail.com>
2019-03-18 12:30:51 +0000

    Tests for successReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,6 @@
-import { combineReducers } from 'redux'
-import success from './successReducer'
-export default combineReducers({ success })
+import { combineReducers } from 'redux';
+import success from './successReducer';
+
+export default combineReducers({
+    success,
+});
--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,16 @@
-export default (state, action) => {
-    return null
+import { actionTypes } from '../actions';
+
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+export default (state=false, action) => {
+  switch(action.type) {
+    case (actionTypes.CORRECT_GUESS):
+      return true;
+    default:
+      return state;
+  }
 }
