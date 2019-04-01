5322009d5574c599c57893e72ac37bae7a771984
RichardLiao <bnmghjtyu0@gmail.com>
2019-04-01 07:13:49 +0000

    33

--- src/Button/__test__/Mock.test.js
+++ src/Button/__test__/Mock.test.js
@@ -16,6 +16,7 @@ test('使用jest.spyOn()监控fetch.fetchPostsList被正常调用', async () => 
     const wrapper = setup()
     const spyFn = jest.spyOn(fetch, 'fetchPostsList');
     await wrapper.instance().getAllGoods();
+    // console.log(wrapper.instance().state.datas)
     expect(spyFn).toHaveBeenCalled();
     expect(spyFn).toHaveBeenCalledTimes(1);
 })
