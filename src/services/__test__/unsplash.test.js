562b70b68fda147f67f3227e769758194684cf78
RichardLiao <bnmghjtyu0@gmail.com>
2019-04-03 06:31:08 +0000

    ok

--- src/services/__test__/unsplash.test.js
+++ src/services/__test__/unsplash.test.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import MockAdapter from 'axios-mock-adapter'
 import unsplash from '../unsplash';
 
 describe('unsplash', () => {
-    it('returns data when sendMessage is called', done => {
+    test('returns data when sendMessage is called', done => {
+
+        // 使用 MockAdapter 可以建立假的 get api
         var mock = new MockAdapter(axios);
         const data = { results: ["cat.jpg"] }
         mock.onGet('https://api.unsplash.com/search/photos').reply(200, data);
+
+        // axios.get 上面連結，回傳 data 的值
+        axios.get('https://api.unsplash.com/search/photos')
+            .then(res => {
+                console.log(res)
+            })
+
         unsplash.sendMessage('cats').then(response => {
             expect(response).toEqual(data);
             done();
